6abe9cefed419ee589dd476a9a65b365c4978d17
jiangqb <jiangqb@citycloud.com.cn>
2021-08-27 15:04:25 +0800

    feat(modified): modified

--- ui-starter-new/src/view/login/login.vue
+++ ui-starter-new/src/view/login/login.vue
@@ -1,115 +1,134 @@
 <style lang="less">
-  @import './login.less';
+@import "./login.less";
 </style>
 
 <template>
   <div class="login" @keydown.enter="handleSubmit">
-    <div style="padding-left: 5%; padding-top: 20%; width: 100%;height: 100%">
+    <!-- <div style="padding-left: 5%; padding-top: 20%; width: 100%;height: 100%">
       <h1 style="color:#cecece;font-family: Georgia; ">MoliCode，Tech make life better</h1>
-    </div>
+    </div> -->
     <div class="login-con">
       <Card :bordered="false">
         <p slot="title">
           <Icon type="log-in"></Icon>
-          欢迎登录MoliCode
+          欢迎登录
         </p>
         <div class="form-con">
           <Form ref="loginForm" :model="form" :rules="rules">
             <FormItem prop="userName">
               <Input v-model="form.userName" placeholder="请输入用户名">
-              <span slot="prepend">
-                                    <Icon :size="16" type="person"></Icon>
-                                </span>
+                <span slot="prepend">
+                  <Icon :size="16" type="person"></Icon>
+                </span>
               </Input>
             </FormItem>
             <FormItem prop="password">
-              <Input type="password" v-model="form.password" placeholder="请输入密码">
-              <span slot="prepend">
-                                    <Icon :size="14" type="locked"></Icon>
-                                </span>
+              <Input
+                type="password"
+                v-model="form.password"
+                placeholder="请输入密码"
+              >
+                <span slot="prepend">
+                  <Icon :size="14" type="locked"></Icon>
+                </span>
               </Input>
             </FormItem>
             <FormItem>
-              <Button @click="handleSubmit" type="primary" long :loading="loading">登录</Button>
-              <br/>
-              <Button @click="register" type="default" long :loading="loading">注册</Button>
+              <Button
+                @click="handleSubmit"
+                type="primary"
+                long
+                :loading="loading"
+                >登录</Button
+              >
+              <br />
+              <Button @click="register" type="default" long :loading="loading"
+                >注册</Button
+              >
             </FormItem>
           </Form>
-          <p class="login-tip">默认用户名：admin, 默认密码：molicodepwd<br/> 如果已调整请记得密码！</p>
+          <!-- <p class="login-tip">
+            默认用户名：admin, 默认密码：molicodepwd<br />
+            如果已调整请记得密码！
+          </p> -->
         </div>
       </Card>
     </div>
 
     <register ref="register"></register>
-
   </div>
 </template>
 
 <script>
-  import constants from '@/constants/constants'
-  import requestUtils from '@/request/requestUtils.js'
-  import register from '../user/register'
-  import { mapActions } from 'vuex'
+import constants from "@/constants/constants";
+import requestUtils from "@/request/requestUtils.js";
+import register from "../user/register";
+import { mapActions } from "vuex";
 
-  var _ = require('underscore')
+var _ = require("underscore");
 
-  export default {
-    data () {
-      return {
-        form: {
-          userName: 'admin',
-          password: ''
-        },
-        rules: {
-          userName: [
-            { required: true, message: '账号不能为空', trigger: 'blur' }
-          ],
-          password: [
-            { required: true, message: '密码不能为空', trigger: 'blur' }
-          ]
-        },
-        loading: false
-      }
-    },
-    methods: {
-      handleSubmit () {
-        let _this = this
-        this.$refs.loginForm.validate((valid) => {
-          if (!valid) {
-            return
-          }
-          var datas = requestUtils.serializeObject(_this.form, true, true)
-          requestUtils.postSubmit(_this, constants.urls.loginfree.login.login, datas, function (data) {
-            this.$store.commit('setLoginUser', { 'userInfo': data.value, vm: this })
+export default {
+  data() {
+    return {
+      form: {
+        userName: "admin",
+        password: ""
+      },
+      rules: {
+        userName: [
+          { required: true, message: "账号不能为空", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "密码不能为空", trigger: "blur" }]
+      },
+      loading: false
+    };
+  },
+  methods: {
+    handleSubmit() {
+      let _this = this;
+      this.$refs.loginForm.validate(valid => {
+        if (!valid) {
+          return;
+        }
+        var datas = requestUtils.serializeObject(_this.form, true, true);
+        requestUtils.postSubmit(
+          _this,
+          constants.urls.loginfree.login.login,
+          datas,
+          function(data) {
+            this.$store.commit("setLoginUser", {
+              userInfo: data.value,
+              vm: this
+            });
             _this.$Message.success({
-              content: '登录成功，欢迎使用MoliCode',
+              content: "登录成功！",
               duration: 10
-            })
+            });
             this.$router.push({
               name: this.$config.homeName
-            })
-          }, function (data) {
-            var sMessage = data['message']
-            if (!sMessage || sMessage === '') {
-              sMessage = '处理异常，请联系管理员！'
+            });
+          },
+          function(data) {
+            var sMessage = data["message"];
+            if (!sMessage || sMessage === "") {
+              sMessage = "处理异常，请联系管理员！";
             }
             _this.$Message.error({
               content: sMessage,
               duration: 10
-            })
-          })
-        }, true)
-      },
-      register () {
-        this.$refs.register.show(true)
-      }
+            });
+          }
+        );
+      }, true);
     },
-    components: {
-      register
+    register() {
+      this.$refs.register.show(true);
     }
+  },
+  components: {
+    register
   }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- ui-starter-new/src/view/single-page/home/home.vue
+++ ui-starter-new/src/view/single-page/home/home.vue
@@ -1,5 +1,5 @@
 <style lang="less">
-  @import "./home.less";
+@import "./home.less";
 </style>
 
 <template>
@@ -7,32 +7,45 @@
     <Row :gutter="10">
       <Col :md="20" :lg="20">
         <Row class-name="home-page-row1" :gutter="10">
-
-          <Col :md="24" :lg="24" :style="{marginBottom: '10px'}">
+          <Col :md="24" :lg="24" :style="{ marginBottom: '10px' }">
             <Card>
               <Row type="flex" class="user-infor">
                 <Col span="8">
-                  <Row class-name="made-child-con-middle" type="flex" align="middle">
-                    <img class="avator-img" src="../../../images/moli.jpg"/>
+                  <Row
+                    class-name="made-child-con-middle"
+                    type="flex"
+                    align="middle"
+                  >
+                    <img class="avator-img" src="../../../images/moli.jpg" />
                   </Row>
                 </Col>
                 <Col span="16" style="padding-left:6px;">
-                  <Row class-name="made-child-con-middle" type="flex" align="middle">
+                  <Row
+                    class-name="made-child-con-middle"
+                    type="flex"
+                    align="middle"
+                  >
                     <div>
-                      <b class="card-user-infor-name">MoliCode</b>
+                      <b class="card-user-infor-name">代码生成器</b
+                      ><a
+                        href="http://doc.dc.citycloud.com.cn/project-5/doc-11/"
+                        target="_blank"
+                        >（使用手册）</a
+                      >
                       <p>welcome admin~</p>
                     </div>
                   </Row>
                 </Col>
               </Row>
               <div class="line-gray"></div>
-              <Row class="margin-top-8">
-                <Col span="8"><p class="notwrap">欢迎使用:</p></Col>
-                <Col span="16" class="padding-left-8">MoliCode 自动代码生成器！</Col>
-              </Row>
+              <!-- <Row class="margin-top-8">
+                <Col span="8"
+                  ><p class="notwrap">欢迎使用代码生成器平台:</p>
+                  </Col
+                >
+              </Row> -->
             </Card>
           </Col>
-
         </Row>
       </Col>
     </Row>
@@ -40,13 +53,12 @@
 </template>
 
 <script>
-
-  export default {
-    name: 'home',
-    components: {},
-    data () {
-      return {}
-    },
-    methods: {}
-  }
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
